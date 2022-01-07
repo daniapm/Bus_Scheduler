e92ba1a98fbfed8b677118ecadc58be75f639bf5
daniapm <daniajibellis@hotmail.com>
2022-01-07 17:41:05 -0500

    Adding Entity

---
+++ app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -0,0 +1,15 @@
+package com.example.busschedule.database.schedule
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Creamos una entidad o modelo para representar nuestra tabla
+
+@Entity
+data class Schedule(
+    @PrimaryKey val id: Int,
+    @NonNull @ColumnInfo(name = "stop_name") val stopName: String,
+    @NonNull @ColumnInfo(name = "arribal_time") val arrivalTime: Int,
+ )
